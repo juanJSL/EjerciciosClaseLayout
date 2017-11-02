361110cf772fc9ca7fc10a621fb17c0cc9c85a5a
Unknown <jjolula88@gmail.com>
2017-11-02 10:30:50 +0100

    Ejercicio 1, 2 3 sin terminar

---
+++ app/src/main/java/com/hlc/ejerciciosclaselayout/MainActivity.java
@@ -0,0 +1,25 @@
+package com.hlc.ejerciciosclaselayout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirRelativeLayout(View v){
+        Intent i = new Intent(this, RelativeLayout.class);
+        startActivity(i);
+    }
+
+    public void abrirtableLayout(View v){
+        Intent i = new Intent(this, TableLayout.class);
+        startActivity(i);
+    }
+}
